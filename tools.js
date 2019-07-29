24dd4b8751ad7788a125310d102e68cd12ec05df
skazemi <skazemi@csumb.edu>
2019-07-29 01:53:32 -0400

    revert back to heroku mysql server

--- tools.js
+++ tools.js
@@ -62,11 +62,10 @@ module.exports = {
   createConnection: function()
   {
     var conn = mysql.createConnection({
-    //host: "us-cdbr-iron-east-02.cleardb.net",
-    host: "mysql.kazemicode.org",
-    user: "sarkaz",
-    password: "shook5*bouts",
-    database: "kazstudents"
+    host: "us-cdbr-iron-east-02.cleardb.net",
+    user: "bbfb8d889bb3ef",
+    password: "e8440918",
+    database: "heroku_d7d0586aaa04448"
   })
     return conn;
   }
